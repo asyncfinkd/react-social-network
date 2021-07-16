088562a42931cd0fc421830716615e268a1c55b6
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-16 23:35:15 +0400

    Inputs in Login Page

--- src/components/indexNotLogged/login/IndexNotLoggedLogin.js
+++ src/components/indexNotLogged/login/IndexNotLoggedLogin.js
@@ -1,9 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as Styled from "../../styles/indexNotLogged/IndexNotLoggedStyles";
 import IndexNotLoggedFooter from "../footer/IndexNotLoggedFooter";
 import { Link, useLocation } from "react-router-dom";
 
 export default function IndexNotLoggedLogin() {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const submit = () => {
+    if (!inputs.email) {
+      setEmailError(true);
+      setPasswordError(false);
+    } else if (!inputs.password) {
+      setEmailError(false);
+      setPasswordError(true);
+    } else {
+      setEmailError(false);
+      setPasswordError(false);
+    }
+  };
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -17,14 +42,30 @@ export default function IndexNotLoggedLogin() {
             <Styled.GridContainer__ContentTitle>
               Login
             </Styled.GridContainer__ContentTitle>
-            <Styled.GridContainer__ContentInput
-              type="text"
-              placeholder="Email"
-            />
-            <Styled.GridContainer__ContentInput
-              type="password"
-              placeholder="Password"
-            />
+            <div style={{ position: "relative" }}>
+              <Styled.GridContainer__ContentInput
+                type="text"
+                placeholder="Email"
+                value={inputs.email}
+                onChange={handleChange}
+                name="email"
+                autoComplete="off"
+                autoCapitalize="off"
+                autoCorrect="off"
+              />
+            </div>
+            <div style={{ position: "relative" }}>
+              <Styled.GridContainer__ContentInput
+                type="password"
+                placeholder="Password"
+                value={inputs.password}
+                onChange={handleChange}
+                name="password"
+                autoComplete="off"
+                autoCapitalize="off"
+                autoCorrect="off"
+              />
+            </div>
             <Styled.GridContainer__ContentInputButton>
               Login
             </Styled.GridContainer__ContentInputButton>
