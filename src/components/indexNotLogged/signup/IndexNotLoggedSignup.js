527f04f26d855fb51e69c4855e427554e406bd56
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-17 01:08:11 +0400

    Refs, Values, HandleChange

--- src/components/indexNotLogged/signup/IndexNotLoggedSignup.js
+++ src/components/indexNotLogged/signup/IndexNotLoggedSignup.js
@@ -1,9 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import * as Styled from "../../styles/indexNotLogged/IndexNotLoggedStyles";
 import IndexNotLoggedFooter from "../footer/IndexNotLoggedFooter";
 
 export default function IndexNotLoggedSignup() {
+  const [inputs, setInputs] = useState({
+    firstName: "",
+    firstNameRef: useRef(),
+    lastName: "",
+    lastNameRef: useRef(),
+    email: "",
+    emailRef: useRef(),
+    password: "",
+    passwordRef: useRef(),
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -11,17 +28,50 @@ export default function IndexNotLoggedSignup() {
   }, [pathname]);
   return (
     <>
-      <Styled.GridContainer>
-        <Styled.GridContainer__Content>
-          <Styled.GridContainer__ContentTitle>
-            Sign Up
-          </Styled.GridContainer__ContentTitle>
-          <Styled.GridContainerBoth__InputsContainer>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Styled.GridContainer>
+          <Styled.GridContainer__Content>
+            <Styled.GridContainer__ContentTitle>
+              Sign Up
+            </Styled.GridContainer__ContentTitle>
+            <Styled.GridContainerBoth__InputsContainer>
+              <div style={{ position: "relative" }}>
+                <Styled.GridContainerBoth__Input
+                  type="text"
+                  placeholder="First name"
+                  autoFocus
+                  ref={inputs.firstNameRef}
+                  name="firstName"
+                  value={inputs.firstName}
+                  onChange={handleChange}
+                  autoComplete="off"
+                  autoCapitalize="off"
+                  autoCorrect="off"
+                />
+              </div>
+              <div style={{ position: "relative" }}>
+                <Styled.GridContainerBoth__Input
+                  type="text"
+                  placeholder="Last name"
+                  ref={inputs.lastNameRef}
+                  name="lastName"
+                  value={inputs.lastName}
+                  onChange={handleChange}
+                  autoComplete="off"
+                  autoCapitalize="off"
+                  autoCorrect="off"
+                />
+              </div>
+            </Styled.GridContainerBoth__InputsContainer>
             <div style={{ position: "relative" }}>
               <Styled.GridContainerBoth__Input
+                style={{ width: "100%" }}
                 type="text"
-                placeholder="First name"
-                autoFocus
+                placeholder="Email address"
+                ref={inputs.emailRef}
+                name="email"
+                value={inputs.email}
+                onChange={handleChange}
                 autoComplete="off"
                 autoCapitalize="off"
                 autoCorrect="off"
@@ -29,40 +79,25 @@ export default function IndexNotLoggedSignup() {
             </div>
             <div style={{ position: "relative" }}>
               <Styled.GridContainerBoth__Input
-                type="text"
-                placeholder="Last name"
+                style={{ width: "100%" }}
+                type="password"
+                placeholder="New password"
+                ref={inputs.passwordRef}
+                name="password"
+                value={inputs.password}
+                onChange={handleChange}
                 autoComplete="off"
                 autoCapitalize="off"
                 autoCorrect="off"
               />
             </div>
-          </Styled.GridContainerBoth__InputsContainer>
-          <div style={{ position: "relative" }}>
-            <Styled.GridContainerBoth__Input
-              style={{ width: "100%" }}
-              type="text"
-              placeholder="Email address"
-              autoComplete="off"
-              autoCapitalize="off"
-              autoCorrect="off"
-            />
-          </div>
-          <div style={{ position: "relative" }}>
-            <Styled.GridContainerBoth__Input
-              style={{ width: "100%" }}
-              type="password"
-              placeholder="New password"
-              autoComplete="off"
-              autoCapitalize="off"
-              autoCorrect="off"
-            />
-          </div>
-          <Styled.GridContainer__ContentInputButton>
-            Sign Up
-          </Styled.GridContainer__ContentInputButton>
-        </Styled.GridContainer__Content>
-        <IndexNotLoggedFooter />
-      </Styled.GridContainer>
+            <Styled.GridContainer__ContentInputButton>
+              Sign Up
+            </Styled.GridContainer__ContentInputButton>
+          </Styled.GridContainer__Content>
+          <IndexNotLoggedFooter />
+        </Styled.GridContainer>
+      </form>
     </>
   );
 }
