9261e5f088c6518c0bfb7c9920a2971a67c6e632
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-26 14:37:27 +0400

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Open up App.js to starts working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0b0e11",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
   },
 });
