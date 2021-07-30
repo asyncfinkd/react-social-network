99fceb368de833e49c6360144e2afb2748cf776b
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-30 13:53:47 +0400

    Logo

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import DogeIcon from "./src/assets/icons/DogeIcon";
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <Text>Open up App.js to starts working on your app!</Text>
+        <DogeIcon />
         <StatusBar style="auto" />
       </View>
     </>
